1f3527ddf1cfdfb01250c293657bb3e4b53c2f87
victor <victorpaul.war@gmail.com>
2018-08-21 11:16:39 +0300

    added ability to assign port

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
+const portPattern = /^port=(\d+)$/;
 var port = 8080;
+
+process.argv.forEach(function (val, index, array) {
+    console.log(index + ': ' + val);
+    var r = portPattern.exec(val);
+    if (r) {
+        port = r[1];
+        console.log('assign new port ' + port);
+    }
+});
 
 var connect = require('connect');
 var serveStatic = require('serve-static');
